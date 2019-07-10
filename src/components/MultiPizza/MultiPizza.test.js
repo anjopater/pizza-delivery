28f29719646667c8721ee6242ec842bd6c36cd9b
antonio <apaterninajc@gmail.com>
2019-07-10 18:32:01 -0300

    added chai

--- src/components/MultiPizza/MultiPizza.test.js
+++ src/components/MultiPizza/MultiPizza.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
 import MultiPizza from './MultiPizza';
 
 describe('MultiPizza', () => {
     it('should render', () => {
         const wrapper = shallow(<MultiPizza />);
-        expect(wrapper.find('MultiPizza').length).toEqual(1);
+        console.log('hey',wrapper.find('MultiPizza'));
+       // expect(wrapper.find(MultiPizza)).to.have.lengthOf(1);
     });
 });
